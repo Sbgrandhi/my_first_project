f1ea5bade89956e0856afa77eacfc3e888fd99cf
Sbgrandhi <sbgrandhi2000@gmail.com>
2025-08-03 12:51:50 +0530

    Update variables.tf

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -4,3 +4,14 @@ variable "aws_region" {
     default     = "us-west-2"
 }
 
+variables "ami_id" {
+    description = "The AMI ID to use for the EC2 instances"
+    type        = string
+    default     = "ami-08a6efd148b1f7504" # Example AMI ID, replace with your own
+}
+
+variable "instance_type" {
+    description = "The type of EC2 instance to launch"
+    type        = string
+    default     = "t3.micro"
+}
